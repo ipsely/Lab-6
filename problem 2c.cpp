6f549de0a8232764982e81d7814b0996fdf3818d
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-11-20 03:09:18 +0530

    Update problem 2c.cpp

--- problem 2c.cpp
+++ problem 2c.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main()
 {
-    int a=2,b=3;
+    int a,b;
     int *p;
+    p = &a;
+    b = *p;
+    cout << "a = " << a << "\nb = " << b << "\n*p = " << *p;
+ 
+    int a=2,b=3;
+    cout << "a = " << a << "\nb = " << b << "\n*p = " << *p;
+    
     p = &b;
    
     cout << "a = " << a << "\nb = " << b << "\n*p = " << *p;
