1ed7ed48db0a37b4f689ddcebc64727464a0a319
Ipsita Priyadarshini <31177734+ipsely@users.noreply.github.com>
2017-11-20 03:10:48 +0530

    Update problem 2c.cpp

--- problem 2c.cpp
+++ problem 2c.cpp
@@ -7,12 +7,12 @@ int main()
     int *p;
     p = &a;
     b = *p;
-    cout << "a = " << a << "\nb = " << b << "\n*p = " << *p;
+    cout << "a = " << a << "\nb = " << b << "\n*p = " << *p<< endl;
  
     int a=2,b=3;
-    cout << "a = " << a << "\nb = " << b << "\n*p = " << *p;
+    cout << "a = " << a << "\nb = " << b << "\n*p = " << *p<< endl;
     
     p = &b;
    
-    cout << "a = " << a << "\nb = " << b << "\n*p = " << *p;
+    cout << "a = " << a << "\nb = " << b << "\n*p = " << *p<< endl;
 }
